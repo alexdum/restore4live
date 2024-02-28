6a7151cc107277224df827eec9e18d59c4428da0
alexdum <alexandru.dumitrescu@gmail.com>
2024-02-28 17:35:09 +0200

    add colors

--- global.R
+++ global.R
@@ -3,8 +3,11 @@ library(bslib)
 library(leaflet)
 library(sf)
 library(terra)
+library(highcharter)
+library(RColorBrewer)
 
-source("utils/leaflet_fun.R")
+source("utils/leaflet_fun.R", local = T)
+source("utils/map_cols_cmip_fun.R", local = T)
 
 dun <- st_read("www/data/shps/restore4life_aoi_etrs89.kml", quiet = T)
 
