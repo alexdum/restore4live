93c1e409e6c46790de7ed3500d75f776d6391fb6
alexdum <alexandru.dumitrescu@gmail.com>
2024-02-28 18:08:16 +0200

    subsetare sezon/luna

--- server.R
+++ server.R
@@ -5,13 +5,19 @@ server <- function(input, output) {
         param <- input$param
         
         season <- strsplit(input$season, "-")[[1]][2]
+        season_subset <- strsplit(input$season, "-")[[1]][1]
+        
         sufix <- ifelse(input$scen %in% "hist", "-50_19601201-20141130.nc", "-50_20150101-21001231.nc")
         
         file <- paste0("www/data/ncs/cmip6/",input$param,"/",input$scen,"/",input$param,"_",input$scen,"_",season, sufix)
         
-        print(file)
-        r <- rast(file) |> mean()
+        r <- rast(file) 
+        dats <- time(r)
         
+        # subseteaza sezoniere si lunare
+        if (season_subset != "year") r <- r[[which(format(dats, "%m") %in% season_subset)]]
+      
+        r <- mean(r)
         setMinMax(r)
         
         pal <- map_cols_cmip_fun(indic = input$param, domain = minmax(r))
@@ -20,23 +26,20 @@ server <- function(input, output) {
         
     }) 
     
-    observe({
-        
-        print(data_sel()$r)
-    })
     
     
-    #functie leaflet de start
+    # functie leaflet de start
     output$map <- renderLeaflet({
         leaflet_fun()
     })
     
+    # update leaflet outputuri
     observe({
-        
         r <- data_sel()$r
         pal <- data_sel()$pal
         min_max <- data_sel()$min_max
         opacy <- data_sel()$opacy
+        
         leafletProxy("map") |>
             clearImages() |>
             addRasterImage(r, opacity = opacy, color = pal$pal) |>
