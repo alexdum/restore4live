f7e27385a476a6408804da2725cf6e266130de52
alexdum <alexandru.dumitrescu@gmail.com>
2024-02-28 15:16:15 +0200

    add selectors

--- global.R
+++ global.R
@@ -9,3 +9,5 @@ source("utils/leaflet_fun.R")
 dun <- st_read("www/data/shps/restore4life_aoi_etrs89.kml", quiet = T)
 
 
+select_seas <- read.csv("www/data/tabs/select_seas.csv")
+select_seas <- setNames(select_seas$choice, select_seas$parameter)
--- ui.R
+++ ui.R
@@ -3,10 +3,28 @@ ui <-
   page_fillable(
     layout_sidebar(
       sidebar = sidebar(
+        selectInput(
+          "param", "Parameter",
+          c("Mean temperature" = "tas",
+            "Maximum temperature" = "tasmax",
+            "Minimum temperature" = "tasmin",
+            "Precipitation" = "prec")
+          ),
+        selectInput(
+          "scen", "Scenario",
+          c("SSP1" = "ssp1",
+            "SSP2" = "ssp2",
+            "SSP3" = "ssp3",
+            "SSP5" = "ssp5")
+        ),
+        selectInput(
+          inputId = "season",
+          label = "Season",
+          choices = select_seas,
+          selected =  select_seas[17]
+        )
       ),
-      
       # Show results
-      
       card(
         full_screen = T,
         leafletOutput("map")
