46d6cccd931115b86a9071864384ddefe33a1f65
alexdum <alexandru.dumitrescu@gmail.com>
2024-03-14 14:07:13 +0200

    add anomalies

--- ui.R
+++ ui.R
@@ -8,34 +8,48 @@ ui <-
           c("Mean temperature" = "tas",
             "Maximum temperature" = "tasmax",
             "Minimum temperature" = "tasmin",
-            "Precipitation" = "pr")
-          ),
+            "Precipitation" = "pr")),
         selectInput(
           "quant", "Quantity",
           c("Climatology" = "climate",
-            "Change" = "change")
-        ),
+            "Change" = "change")),
         selectInput(
           "scen", "Scenario",
-          c("Historical" = "hist",
-            "SSP1" = "ssp1",
+          c("SSP1" = "ssp1",
             "SSP2" = "ssp2",
             "SSP3" = "ssp3",
-            "SSP5" = "ssp5")
-        ),
+            "SSP5" = "ssp5")),
         selectInput(
           inputId = "season",
           label = "Season",
           choices = select_seas,
-          selected =  select_seas[17]
+          selected =  select_seas[17]),
+        # afiseaza inputul conditional
+        conditionalPanel(
+          condition = "input.quant == 'climate'",
+          sliderTextInput(
+            inputId = "period_climate",
+            label = "Climate", 
+            selected = c("1981-2010"),
+            choices = c("1961-1990", "1981-2010", "2021-2040", "2041-2060", "2081-2100")),
+        ),
+        conditionalPanel(
+          condition = "input.quant == 'change'",
+          sliderTextInput(
+            inputId = "period_change",
+            label = "Change", 
+            selected = c("1981-2010", "2021-2040"),
+            choices = c("1961-1990", "1981-2010", "2021-2040", "2041-2060", "2081-2100"),
+            from_min = "1961-1990", 
+            from_max = "1981-2010",
+            to_min =  "2021-2040",
+            to_max = "2081-2100")
         ),
         
         sliderInput(
           "transp", "Transparency",
           min = 0, max = 1, ticks = F,
-          value = 0.8, step = 0.1,
-        )
-      ),
+          value = 0.8, step = 0.1)),
       # Show results
       card(
         full_screen = T,
