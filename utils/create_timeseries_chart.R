451388aa887ae1821ff99d5341c207d997ef5ea9
alexdum <alexandru.dumitrescu@gmail.com>
2026-01-23 16:07:33 +0200

    improve plots functinalities

--- utils/create_timeseries_chart.R
+++ utils/create_timeseries_chart.R
@@ -1,9 +1,8 @@
 create_timeseries_chart <- function(data_input, param, params_def, is_anomaly = FALSE) {
-  
   param_info <- params_def[params_def$input == param, ]
   param_name <- param_info$parm
   param_unit <- param_info$unit
-  
+
   if (is_anomaly) {
     y_label <- if (param == "pr") "Anomaly (%)" else paste0("Anomaly (", param_unit, ")")
     series_name <- "Anomaly"
@@ -21,25 +20,42 @@ create_timeseries_chart <- function(data_input, param, params_def, is_anomaly = 
     low_col <- "value10"
     high_col <- "value90"
   }
-  
-  col_line <- ifelse(param %in% c("pr","csdi"), "blue", "red")
-  
+
+  col_line <- ifelse(param %in% c("pr", "csdi"), "blue", "red")
+
   if (!is.character(data_input)) {
     # Calculate trendline using linear regression
     trendline <- lm(data_input[[value_col]] ~ as.numeric(data_input$date), data = data_input)
     data_input$trendline <- predict(trendline) |> round(3)
-    
+
     highchart() %>%
       hc_xAxis(categories = format(data_input$date, "%Y")) %>%
       hc_yAxis(title = list(text = y_label)) %>%
-      hc_add_series(color = col_line, name = series_name, data = data_input[[value_col]], type = 'line', marker = list(enabled = FALSE)) %>%
-      hc_add_series(name = "P10 - P90", data = list_parse2(data.frame(low = data_input[[low_col]], high = data_input[[high_col]])), type = 'arearange', color = '#CCCCCC', lineWidth = 0, fillOpacity = 0.3, zIndex = 0) |>
-      hc_add_series(name = "Trendline", data = data_input$trendline, type = 'line', color = 'grey', dashStyle = 'shortdash')
-      
+      hc_add_series(color = col_line, name = series_name, data = data_input[[value_col]], type = "line", marker = list(enabled = FALSE)) %>%
+      hc_add_series(name = "P10 - P90", data = list_parse2(data.frame(low = data_input[[low_col]], high = data_input[[high_col]])), type = "arearange", color = "#CCCCCC", lineWidth = 0, fillOpacity = 0.3, zIndex = 0) %>%
+      hc_add_series(name = "Trendline", data = data_input$trendline, type = "line", color = "grey", dashStyle = "shortdash") %>%
+      hc_exporting(
+        enabled = TRUE,
+        buttons = list(
+          contextButton = list(
+            menuItems = list(
+              "downloadPNG",
+              "downloadJPEG",
+              "downloadPDF",
+              "downloadSVG",
+              "separator",
+              "downloadCSV",
+              "downloadXLS"
+            )
+          )
+        )
+      )
   } else {
-    highchart() |> 
+    highchart() |>
       hc_title(
         text = data_input,
-        style = list(fontSize = "14px", color = "grey"))
+        style = list(fontSize = "14px", color = "grey")
+      ) |>
+      hc_exporting(enabled = TRUE)
   }
 }
